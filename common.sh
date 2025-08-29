ff64e4a63e98a1d79ad586078b24df4b03da69c3
Makayden Lofthouse <makayden.lofthouse@gmail.com>
2025-08-28 19:45:33 -0600

    Update link script slightly

---
+++ common.sh
@@ -0,0 +1,81 @@
+#!/usr/bin/sh
+
+for arg
+do
+    if [ "${arg}" = "--dry" -o "${arg}" = "-d" ]
+    then
+        dry="TRUE"
+    else
+        echo "Argument ${arg} not recognized"
+        exit 1
+    fi
+done
+
+if [ "${dry}" = "TRUE" ]
+then
+    echo "Dry run..."
+fi
+
+# If using any shell special symbols like < or > (or others not listed here),
+# make sure you pass them to run_cmd_always with single quotes, i.e. '<' or '>'.
+run_cmd_always () {
+    eval "${@}"
+    if [ "${?}" -ne "0" ]
+    then
+        echo "Failed to run command: ${@}"
+        exit "${?}"
+    fi
+}
+
+# If using any shell special symbols like < or > (or others not listed here),
+# make sure you pass them to run_cmd with single quotes, i.e. '<' or '>'.
+run_cmd () {
+    if [ "${dry}" = "TRUE" ]
+    then
+        echo "${@}"
+    else
+        run_cmd_always "${@}"
+    fi
+}
+
+symlinks_created="0"
+symlinks_replaced="0"
+
+# $1: link target, $2: link name
+replace_with_symlink() {
+    echo "Removing ${2}; not a link to ${1}"
+    run_cmd rm "${2}"
+
+    echo "Symlinking ${2} to ${1}"
+    run_cmd ln -s "${1}" "${2}"
+    symlinks_replaced=$((symlinks_replaced+1))
+    echo ""
+}
+
+# $1: path for file to link to
+# $2: final desination for link
+safe_link() {
+    if [ -L "${2}" ]
+    then
+        realdest=$(run_cmd_always realpath -m "${2}")
+        if [ "${realdest}" != "${1}" ]
+        then
+            replace_with_symlink "${1}" "${2}"
+        fi
+    elif [ -f "${2}" ]
+    then
+        replace_with_symlink "${1}" "${2}"
+    else
+        echo "Symlinking ${2} to ${1}"
+        run_cmd ln -s "${1}" "${2}"
+        symlinks_created=$((symlinks_created+1))
+        echo ""
+    fi
+}
+
+if [ -z "${XDG_CONFIG_HOME:-}" ]
+then
+    mpv_dir="${HOME}/.config/mpv"
+else
+    mpv_dir="${XDG_CONFIG_HOME}/mpv"
+fi
--- link.sh
+++ link.sh
@@ -1,57 +1,8 @@
 #!/usr/bin/sh
 
-for arg
-do
-    if [ "${arg}" = "--dry" -o "${arg}" = "-d" ]
-    then
-        dry="TRUE"
-    else
-        echo "Argument ${arg} not recognized"
-        exit 1
-    fi
-done
-
-if [ "${dry}" = "TRUE" ]
-then
-    echo "Dry run..."
-fi
-
-# If using any shell special symbols like < or > (or others not listed here),
-# make sure you pass them to run_cmd_always with single quotes, i.e. '<' or '>'.
-run_cmd_always () {
-    eval "${@}"
-    if [ "${?}" -ne "0" ]
-    then
-        echo "Failed to run command: ${@}"
-        exit "${?}"
-    fi
-}
-
-# If using any shell special symbols like < or > (or others not listed here),
-# make sure you pass them to run_cmd with single quotes, i.e. '<' or '>'.
-run_cmd () {
-    if [ "${dry}" = "TRUE" ]
-    then
-        echo "${@}"
-    else
-        run_cmd_always "${@}"
-    fi
-}
+. ./common.sh
 
 dirs_made="0"
-symlinks_created="0"
-symlinks_replaced="0"
-
-# $1: link target, $2: link name
-replace_with_symlink() {
-    echo "Removing ${2}; not a link to ${1}"
-    run_cmd rm "${2}"
-
-    echo "Symlinking ${2} to ${1}"
-    run_cmd ln -s "${1}" "${2}"
-    symlinks_replaced=$((symlinks_replaced+1))
-    echo ""
-}
 
 # $1: repo dir to traverse
 # $2: repo to link files into
@@ -78,27 +29,10 @@ ln_dir () {
 
         dest_file="${dest_dir}/${file_base}"
 
-        if [ -L "${dest_file}" ]
-        then
-            realdest=$(run_cmd_always realpath -m "${dest_file}")
-            if [ "${realdest}" != "${realfile}" ]
-            then
-                replace_with_symlink "${realfile}" "${dest_file}"
-            fi
-        elif [ -f "${dest_file}" ]
-        then
-            replace_with_symlink "${realfile}" "${dest_file}"
-        else
-            echo "Symlinking ${dest_file} to ${realfile}"
-            run_cmd ln -s "${realfile}" "${dest_file}"
-            symlinks_created=$((symlinks_created+1))
-            echo ""
-        fi
+        safe_link "${realfile}" "${dest_file}"
     done
 }
 
-
-mpv_dir="${HOME}/.config/mpv"
 ln_dir "./animecards" "${mpv_dir}/scripts/animecards"
 ln_dir "./script-opts" "${mpv_dir}/script-opts"
 
